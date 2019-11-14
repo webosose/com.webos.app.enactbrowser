60fff1b68cad418d6807d27ac1734c05d2b5f624
Seungcheon Baek <sc.baek@lge.com>
2019-11-14 16:23:59 +0900

    [Enact Browser] Show info if web page become unresponsive
    :Release Notes:
    When a web page becomes unresponsive, a browser shows a dialog for a
    user to wait or to stop the page.
    
    :Detailed Notes:
    A dialog is added for a user to decide to wait until a page becomes
    responsive in case that an unresponsive web page is loaded.
    
    :Testing Performed:
    1. Build browser
    2. Start a web server to simulate an unresponsive web page.
       (Refer test_pages/unresponsive_page/index.html)
    3. Run the browser and access the page.
    
    :QA Notes:
    
    :Issues Addressed:
    [PLAT-72695] Show info if web page become unresponsive
    
    Change-Id: Ie82c3d09665b6ef93078e0c2a793bbe949bb9b28

--- samples/enact-based/src/views/WebView/WebView.js
+++ samples/enact-based/src/views/WebView/WebView.js
@@ -15,7 +15,15 @@ import PropTypes from 'prop-types';
 import React, {Component} from 'react';
 import ErrorPage from '../ErrorPage';
 
-const WebViewWrapperId = '_webview';
+import $L from '@enact/i18n/$L';
+import Button from '@enact/moonstone/Button';
+import Notification from '@enact/moonstone/Notification';
+
+const
+	WebViewWrapperId = '_webview',
+	error_renderer_crashed = 'RENDERER_CRASHED',
+	error_unresponsive = 'PAGE_UNRESPONSIVE',
+	timeoutToSuppressDialog = 30000;
 
 class WebView extends Component {
 	static propTypes = {
@@ -23,57 +31,123 @@ class WebView extends Component {
 		webView: PropTypes.object
 	}
 
-	showHideWebview = () => {
-		let tab = this.props.tabs[this.props.id];
-		let err = tab.error;
-		let id_ = this.props.id + WebViewWrapperId;
-		let use_chrom_err_page = this.props.browser.config.useBuiltInErrorPages;
+	constructor (props) {
+		super(props);
 
-		let show_webview = err === null || use_chrom_err_page
-			|| err === 'RENDERER_CRASHED' || err === 'PAGE_UNRESPONSIVE';
-		let show_error = !show_webview;
-
-		let webview_elem = document.getElementById(id_);
-		let errpage_elem = document.getElementById(id_ + "errorPage");
-
-		if (webview_elem !== null) {
-			if (show_webview) {
-				webview_elem.removeAttribute("hidden");
-			} else {
-				webview_elem.setAttribute("hidden", "");
-			}
-		}
-
-		if (errpage_elem !== null) {
-			if (show_error) {
-				errpage_elem.removeAttribute("hidden");
-			} else {
-				errpage_elem.setAttribute("hidden", "");
-			}
-		}
+		this.state = {
+			hideDialog: true,
+			hideErrorPage: true,
+			hideWebview: true,
+			ready: false, // to be set to `true` after `loadcommit` event
+			suppressDialog: false
+		};
 	}
 
-	onLoadCommit() {
-		this.showHideWebview();
+	static getDerivedStateFromProps = (nextProps, prevState) => {
+		let
+			{browser, id, tabs} = nextProps,
+			{ready} = prevState,
+			error = tabs[id].error;
+
+		if (ready) {
+			if (error === null) {
+				return {
+					suppressDialog: false,
+					hideDialog: true,
+					hideErrorPage: true,
+					hideWebview: false
+				};
+			} else if (error === error_unresponsive) {
+				if (prevState.suppressDialog) {
+					return {
+						hideDialog: true,
+						hideErrorPage: true,
+						hideWebview: false
+					};
+				} else {
+					return {
+						hideDialog: false,
+						hideErrorPage: true,
+						hideWebview: false
+					};
+				}
+			} else if (error === error_renderer_crashed || browser.config.useBuiltInErrorPages) {
+				return {
+					hideDialog: true,
+					hideErrorPage: true,
+					hideWebview: false
+				};
+			} else {
+				return {
+					hideDialog: true,
+					hideErrorPage: false,
+					hideWebview: true
+				};
+			}
+		}
+
+		return null;
+	}
+
+	onLoadCommit = () => {
+		this.setState({ready: true});
+	}
+
+	enableDialog = () => {
+		this.setState({suppressDialog: false});
+	}
+
+	onWait = () => {
+		setTimeout(this.enableDialog, timeoutToSuppressDialog);
+		this.setState({suppressDialog: true});
+	}
+
+	onStop = () => {
+		this.props.webView.deactivate();
+		this.setState({
+			hideDialog: true,
+			hideErrorPage: false,
+			hideWebview: true,
+			ready: false
+		});
 	}
 
 	componentDidMount () {
 		this.props.webView.insertIntoDom(this.props.id + WebViewWrapperId);
-		this.props.webView.addEventListener('loadcommit', this.onLoadCommit.bind(this));
+		this.props.webView.addEventListener('loadcommit', this.onLoadCommit);
 	}
 
 	render () {
-		const {id, tabs, style, ...rest} = this.props;
+		const
+			{id, tabs, style, ...rest} = this.props,
+			{hideDialog, hideErrorPage, hideWebview} = this.state;
 
 		delete rest.browser;
 		delete rest.webView;
 
-		let id_ = id + WebViewWrapperId;
 		return (
 			<div>
-				<ErrorPage id={id_ + "errorPage"} style={style} hidden
-					errorMsg={tabs[id].error} />
-				<div style={style} hidden id={id_} {...rest} />
+				<div
+					id={id + WebViewWrapperId}
+					style={style}
+					{...rest}
+					hidden={hideWebview}
+				/>
+				<ErrorPage
+					style={style}
+					errorMsg={tabs[id].error}
+					hidden={hideErrorPage}
+				/>
+				<Notification
+					noAutoDismiss
+					open={!hideDialog}
+				>
+					<p>{$L('The current page has become unresponsive. You can wait for it to become responsive.')}</p>
+					<buttons>
+						<Button onClick={this.onWait}>{$L('Wait')}</Button>
+						<Button onClick={this.onStop}>{$L('Stop')}</Button>
+					</buttons>
+				</Notification>
 			</div>
 		);
 	}
