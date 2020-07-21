fc02e2b40c73e70b8f8170b495be00d52d73e2d3
Alexander Lapshenkov <alexander.lapshenkov@lge.com>
2020-07-21 17:42:27 +0300

    [browser] Allow 'media' permission request
    :Release Notes:
    Allowed 'media' permissions request by default without UI
    
    :Detailed:
    Allowed 'media' permissions request by default without UI
    
    :Test Scenario:
    1. Ensure that web camera connected and is accessible
    2. Start enact-based browser
    3. navigate to lgwebrtc.com
    4. Check that video stream from web camera is shown
    
    :QA Notes:
    
    :Issues Addressed:
    [NEVA-5108] Implement site by site permissions support
    
    Change-Id: I6c3879e8084077e2c89113bf27afa2641c4b8bc9
    Reviewed-on: http://repo.lge.com:8080/3618
    BUILD: Alexander Lapshenkov <alexander.lapshenkov@lge.com>
    BUILD-SF: Alexander Lapshenkov <alexander.lapshenkov@lge.com>
    PC-BUILD: test bts <test.bts@lge.com>
    PC-BVT: test bts <test.bts@lge.com>
    Reviewed-by: <stanislav.pikulik@lge.com>
    Reviewed-by: hyojin22.song <hyojin22.song@lge.com>
    BUILD-SF: <ivan.murashov@lge.com>
    STARFISH-BUILD: test bts <test.bts@lge.com>
    STARFISH-BVT: test bts <test.bts@lge.com>
    Release-Notes-Ok: <ivan.murashov@lge.com>
    Tags-Ok: <ivan.murashov@lge.com>
    Tested-by: Alexander Lapshenkov <alexander.lapshenkov@lge.com>
    Reviewed-by: <ivan.murashov@lge.com>

--- src/BrowserBase.js
+++ src/BrowserBase.js
@@ -336,6 +336,9 @@ class BrowserBase {
             case 'fullscreen':
                 ev.request.allow();
                 break;
+            case 'media':
+                ev.request.allow();
+                break;
             default:
                 console.warn("Permission request recieved: " + ev.permission);
                 ev.request.deny();
