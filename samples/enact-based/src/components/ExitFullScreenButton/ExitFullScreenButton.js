5e240c32303dcf6173965a75ce5d2cbd354acd5c
jeongran.jang <jeongran.jang@lge.com>
2020-09-04 15:36:43 +0900

    [Enact Browser] Change the way to exit fullscreen mode
    :Release Notes:
    Change the way to exit fullscreen mode
    
    :Detailed Notes:
    The position of the button to exit fullscreen mode will be fixed
    without animation effect.
    
    :Testing Performed:
    1. Launch web browser
    2. Tap fullscreen button
    3. Tap a button to exit fullscreen after fullscreen mode
    4. Back to normal mode
    
    :QA Notes:
    
    :Issues Addressed:
    [PLAT-114143] Exit fullscreen on a touch screen
    
    Change-Id: Ia697ac7ed820af6e717c09fb1a0cb0740fd7fa5b

--- samples/enact-based/src/components/ExitFullScreenButton/ExitFullScreenButton.js
+++ samples/enact-based/src/components/ExitFullScreenButton/ExitFullScreenButton.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2018-2019 LG Electronics, Inc.
+// Copyright (c) 2018-2020 LG Electronics, Inc.
 // SPDX-License-Identifier: LicenseRef-EnactBrowser-Evaluation
 //
 // You may not use this content except in compliance with the License.
@@ -12,83 +12,45 @@
  */
 
 import $L from '@enact/i18n/$L';
-import {Button} from '@enact/moonstone/Button';
+import Item from '@enact/moonstone/Item';
+import TooltipDecorator from '@enact/moonstone/TooltipDecorator';
 import PropTypes from 'prop-types';
 import React, {Component} from 'react';
-import {Job} from '@enact/core/util';
 
 import css from './ExitFullScreenButton.module.less';
 
+import icon from '../../../assets/default/exit_fullscreen.svg';
+
+const TooltipItem = TooltipDecorator(Item);
+
 class ExitFullScreenButton extends Component {
-	static props = {
+	static propTypes = {
 		fullScreen: PropTypes.bool,
 		onExitFullScreen: PropTypes.func
-	}
+	};
 
 	constructor (props) {
 		super(props);
-		this.state = {
-			animation: css.show
-		};
-	}
-
-	componentDidMount () {
-		this.startHideExitFullScreen.start();
-	}
-
-	componentWillUnmount() {
-		this.startHideExitFullScreen.stop();
-	}
-
-	componentWillReceiveProps (nextProps) {
-		if (nextProps.fullScreen) {
-			this.show();
-			this.startHideExitFullScreen.start();
-		} else {
-			this.startHideExitFullScreen.stop();
-			this.hide();
-		}
-	}
-
-	show = () => {
-		this.setState({animation: css.show});
-	}
-
-	hide = () => {
-		this.setState({animation: css.hide});
-	}
-
-	startHideExitFullScreen = new Job(this.hide, 4000);
-
-	onMouseEnter = () => {
-		if (this.props.fullScreen) {
-			this.show();
-		}
-	}
-
-	onMouseLeave = () => {
-		this.startHideExitFullScreen.start();
 	}
 
 	render () {
-		const
-			{animation} = this.state,
-			{fullScreen} = this.props;
+		const {fullScreen, onExitFullScreen} = this.props;
 
 		return (
-			<div className={css.topArea} onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave}>
-				{
-					fullScreen ?
-					<Button
-						className={animation}
-						onClick={this.props.onExitFullScreen}
-						onMouseEnter={this.onMouseEnter}
-					>
-					{$L('Exit Full Screen')}
-					</Button>
-					: null
-				}
-			</div>
+			fullScreen ?
+				<TooltipItem
+					className={css.item}
+					onClick={onExitFullScreen}
+					tooltipPosition={'right middle'}
+					tooltipText={$L('Exit Full Screen')}
+				>
+					<img
+						alt={$L('Exit Full Screen')}
+						className={css.image}
+						src={icon}
+					/>
+				</TooltipItem> :
+				null
 		);
 	}
 }
